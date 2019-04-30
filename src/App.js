f4e024dbbb0981aa4e93c14477f7c3c588efca50
Cobbie Quintos <cobbiequintos@gmail.com>
2019-04-30 23:09:08 +0800

    timing functionality

--- src/App.js
+++ src/App.js
@@ -2,18 +2,76 @@ import React, { Component } from "react";
 import "./App.css";
 import Button from "./components/Button/Button";
 import Stat from "./components/Stat/Stat";
-import Dropdown from "./components/Dropdown/Dropdown"
+import Dropdown from "./components/Dropdown/Dropdown";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      time: { minutes: 25, seconds: "00" },
+      time: { minutes: 10, seconds: 0 },
       running: false
     };
     this.intervals = 0;
     this.timeElapsed = 0;
+    this.minutes = 0;
+    this.seconds = 0;
+    this.timerID = 0;
   }
+
+  startTimer = () => {
+    this.setState({
+      running: true
+    });
+    this.timerID = setInterval(() => this.tick(), 1000);
+  };
+
+  pauseTimer = () => {
+    this.setState({
+      running: false
+    });
+    clearInterval(this.timerID);
+
+  };
+
+  componentDidMount =() => {
+    if (this.state.running === true) {
+      this.timerID = setInterval(() => this.tick(), 1000);
+    }
+  }
+
+  componentWillUnmount = () => {
+    clearInterval(this.timerID);
+  }
+
+  
+
+  
+  tick = () => {
+    let currentSec = this.state.time.seconds;
+    let currentMin = this.state.time.minutes;
+    let newMin = false;
+
+    if (currentSec === 0) {
+      currentSec = 59;
+      newMin = true;
+    } else {
+      currentSec -= 1;
+    }
+    if (newMin) {
+      currentMin -= 1;
+      newMin = false;
+    }
+    this.setState({
+      time: { minutes: currentMin, seconds: currentSec }
+    });
+  };
+
+  displayProperZero = () => {
+    if (this.state.time.seconds < 10) {
+      return "0" + this.state.time.seconds.toString();
+    }
+    return <span>{this.state.time.seconds}</span>;
+  };
 
   startOrStop = arg => {
     if (arg === false) {
@@ -23,20 +81,24 @@ class App extends Component {
           isGreen={true}
           isSmall={false}
           containsDropdown={false}
+          onClick={this.startTimer}
         />
       );
     }
     return (
-      <Button buttonName="PAUSE" isSmall={false} containsDropdown={false} />
+      <Button
+        buttonName="PAUSE"
+        isSmall={false}
+        containsDropdown={false}
+        onClick={this.pauseTimer}
+      />
     );
   };
 
-  renderDropdown = (bool) => {
-    console.log('asdfg');
-    return(
-      <Dropdown intervalTime={bool}/>
-    )
-  }
+  renderDropdown = bool => {
+    console.log("asdfg");
+    return <Dropdown intervalTime={bool} />;
+  };
 
   render() {
     return (
@@ -59,7 +121,7 @@ class App extends Component {
             </div>
             <div className="timer-flexbox">
               <div className="timer">
-                {this.state.time.minutes}:{this.state.time.seconds}
+                {this.state.time.minutes}:{this.displayProperZero()}
               </div>
               <div className="timer-bar" />
             </div>
